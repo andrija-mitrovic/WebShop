099bebd387e7e8774bc4b1b4bf3c50dc4d638bd0
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-05-16 08:17:25 +0200

    Add product controller

---
+++ WebShop/WebShop.ApiGateway/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using WebShop.Infrastructure.Command.Product;
+using WebShop.Infrastructure.Event.Product;
+using WebShop.Infrastructure.Query.Product;
+
+namespace WebShop.ApiGateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IBusControl _bus;
+        private readonly IConfiguration _configuration;
+        private readonly IRequestClient<GetProductById> _requestClient;
+
+        public ProductController(IBusControl bus, IConfiguration configuration, IRequestClient<GetProductById> requestClient)
+        {
+            _bus = bus;
+            _configuration = configuration;
+            _requestClient = requestClient;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string productId)
+        {
+            var product = new GetProductById() { ProductId = productId };
+            var response = await _requestClient.GetResponse<ProductCreated>(product);
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromForm] CreateProduct product)
+        {
+            var uri = new Uri("rabbitmq://localhost/create_product");
+            var endPoint = await _bus.GetSendEndpoint(uri);
+            await endPoint.Send(product);
+
+            return Ok("Product Created");
+        }
+    }
+}
---
+++ WebShop/WebShop.ApiGateway/Program.cs
@@ -0,0 +1,30 @@
+using WebShop.Infrastructure.EventBus;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+ConfigurationManager configuration = builder.Configuration;
+
+builder.Services.AddControllers();
+builder.Services.AddRabbitMq(configuration);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
