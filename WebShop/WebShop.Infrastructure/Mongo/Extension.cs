eca677b69d4d2aeb047ff53e623048f42bfc33f3
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-05-19 08:16:42 +0200

    Add mongo extension

---
+++ WebShop/WebShop.Infrastructure/Mongo/Extension.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShop.Infrastructure.Mongo
+{
+    public static class Extension
+    {
+        public static void AddMongoDb(this IServiceCollection services, IConfiguration configuration)
+        {
+            var configSection = configuration.GetSection("mongo");
+
+            var mongoConfig = new MongoConfig();
+            configSection.Bind(mongoConfig);
+
+            services.AddSingleton<IMongoClient>(client =>
+            {
+                return new MongoClient(mongoConfig.ConnectionString);
+            });
+            services.AddSingleton<IMongoDatabase>(client =>
+            {
+                var mongoClient = new MongoClient(mongoConfig.ConnectionString);
+                return mongoClient.GetDatabase(mongoConfig.Database);
+            });
+            services.AddSingleton<IDatabaseInitializer, MongoInitializer>();
+        }
+    }
+}
