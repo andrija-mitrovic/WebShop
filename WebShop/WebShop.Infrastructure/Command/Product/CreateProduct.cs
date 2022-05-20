a3ee2d90c97fd32e82e2d313c7d77173737f7dce
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-05-20 19:40:31 +0200

    Add create product command

---
+++ WebShop/WebShop.Infrastructure/Command/Product/CreateProduct.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShop.Infrastructure.Command.Product
+{
+    public class CreateProduct
+    {
+        [BsonId]
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductPrice { get; set; }
+        public Guid CategoryId { get; set; }  
+    }
+}
