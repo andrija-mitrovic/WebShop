22157871d5b607ea558d1fa26b544d407ef8244d
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-05-24 08:52:59 +0200

    Add create product handler

---
+++ WebShop/WebShop.Product.Api/Handlers/CreateProductHandler.cs
@@ -0,0 +1,22 @@
+﻿using MassTransit;
+using WebShop.Infrastructure.Command.Product;
+using WebShop.Product.DataProvider.Services;
+
+namespace WebShop.Product.Api.Handlers
+{
+    public class CreateProductHandler : IConsumer<CreateProduct>
+    {
+        private readonly IProductService _service;
+
+        public CreateProductHandler(IProductService service)
+        {
+            _service = service;
+        }
+
+        public async Task Consume(ConsumeContext<CreateProduct> context)
+        {
+            await _service.AddProduct(context.Message);
+            await Task.CompletedTask;
+        }
+    }
+}
