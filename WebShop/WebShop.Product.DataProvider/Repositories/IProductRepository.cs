0bca5a9d6b07b815358555fa2623a556d524acfc
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-05-25 08:21:54 +0200

    Add product repository

---
+++ WebShop/WebShop.Product.DataProvider/Repositories/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using WebShop.Infrastructure.Command.Product;
+using WebShop.Infrastructure.Event.Product;
+
+namespace WebShop.Product.DataProvider.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<ProductCreated> GetProduct(string productId);
+        Task<ProductCreated> AddProduct(CreateProduct product);
+    }
+}
---
+++ WebShop/WebShop.Product.DataProvider/Repositories/ProductRepository.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Driver;
+using WebShop.Infrastructure.Command.Product;
+using WebShop.Infrastructure.Event.Product;
+
+namespace WebShop.Product.DataProvider.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IMongoDatabase _database;
+        private readonly IMongoCollection<CreateProduct> _collection;
+
+        public ProductRepository(IMongoDatabase database)
+        {
+            _database = database;
+            _collection = database.GetCollection<CreateProduct>("product");
+        }
+
+        public async Task<ProductCreated> AddProduct(CreateProduct product)
+        {
+            await _collection.InsertOneAsync(product);
+            return new ProductCreated { ProductId=product.ProductId, ProductName=product.ProductName, CreatedAt = DateTime.UtcNow };
+        }
+
+        public async Task<ProductCreated> GetProduct(string productId)
+        {
+            var product = _collection.AsQueryable().Where(x => x.ProductId == productId).FirstOrDefault();
+            if (product == null)
+            {
+                throw new Exception("product not found");
+            }
+
+            await Task.CompletedTask;
+            return new ProductCreated { ProductId = product.ProductId, ProductName = product.ProductName, CreatedAt = DateTime.UtcNow };
+        }
+    }
+}
