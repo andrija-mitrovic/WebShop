d4578cc57fc770e86a5d81496bc398d009586347
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-05-26 08:16:50 +0200

    Add product service

---
+++ WebShop/WebShop.Product.DataProvider/Services/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using WebShop.Infrastructure.Command.Product;
+using WebShop.Infrastructure.Event.Product;
+
+namespace WebShop.Product.DataProvider.Services
+{
+    public interface IProductService
+    {
+        Task<ProductCreated> GetProduct(string productId);
+        Task<ProductCreated> AddProduct(CreateProduct product);
+    }
+}
---
+++ WebShop/WebShop.Product.DataProvider/Services/ProductService.cs
@@ -0,0 +1,26 @@
+﻿using WebShop.Infrastructure.Command.Product;
+using WebShop.Infrastructure.Event.Product;
+using WebShop.Product.DataProvider.Repositories;
+
+namespace WebShop.Product.DataProvider.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<ProductCreated> AddProduct(CreateProduct product)
+        {
+            return await _productRepository.AddProduct(product);
+        }
+
+        public async Task<ProductCreated> GetProduct(string productId)
+        {
+            return await _productRepository.GetProduct(productId);
+        }
+    }
+}
