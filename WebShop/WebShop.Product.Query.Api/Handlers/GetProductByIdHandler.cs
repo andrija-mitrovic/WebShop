95a391c104373775531c0f0176a19caf6606f5a7
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-05-27 08:12:01 +0200

    Add get product by id handler

---
+++ WebShop/WebShop.Product.Query.Api/Handlers/GetProductByIdHandler.cs
@@ -0,0 +1,23 @@
+﻿using MassTransit;
+using WebShop.Infrastructure.Event.Product;
+using WebShop.Infrastructure.Query.Product;
+using WebShop.Product.DataProvider.Services;
+
+namespace WebShop.Product.Query.Api.Handlers
+{
+    public class GetProductByIdHandler : IConsumer<GetProductById>
+    {
+        private readonly IProductService _productService;
+
+        public GetProductByIdHandler(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task Consume(ConsumeContext<GetProductById> context)
+        {
+            var product = await _productService.GetProduct(context.Message.ProductId);
+            await context.RespondAsync<ProductCreated>(product);
+        }
+    }
+}
